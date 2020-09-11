72e001c34ab70be990de255f89ed076c956e36c1
modhafferraihane <modhaffer.raihane@gmail.com>
2020-09-11 13:22:03 +0000

    final

---
+++ Back/BooksAPI/Models/BooksDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BooksAPI.Models
+{
+    public partial class BooksDBContext : DbContext
+    {
+        //public BooksDBContext()
+        //{
+        //}
+
+        public BooksDBContext(DbContextOptions<BooksDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+
+        //on peux supprimer OnConfiguring
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("server=.\\SQLSERVER; database=BooksDB; integrated security=true");
+//            }
+//        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Book>(entity =>
+        //    {
+        //        entity.Property(e => e.Author).HasMaxLength(50);
+
+        //        entity.Property(e => e.PublishedAt).HasColumnType("datetime");
+
+        //        entity.Property(e => e.Title).HasMaxLength(50);
+        //    });
+        //}
+    }
+}
